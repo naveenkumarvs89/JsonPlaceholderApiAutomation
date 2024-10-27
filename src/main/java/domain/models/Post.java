2e3585bd5e5c3a6dc7ffa9614d53dd3af3a927c5
Naveenkumar Selvaraj <32835528+naveenkumarvs89@users.noreply.github.com>
2024-10-27 19:03:00 +0530

    Update Post.java

--- src/main/java/domain/models/Post.java
+++ src/main/java/domain/models/Post.java
@@ -41,5 +41,57 @@ public class Post {
         this.id = id;
     }
 
-    // Getters and Setters
+    // Getters and Setterscurl --location 'https://jsonplaceholder.typicode.com/posts' \
+--header 'Content-Type: application/json' \
+--data '{
+    "title": "foo",
+    "body": "bar",
+    "userId": 1
+}'
+{
+    "title": "foo",
+    "body": "bar",
+    "userId": 1,
+    "id": 101
+}
+
+
+
+
+curl --location --request PUT 'https://jsonplaceholder.typicode.com/posts/1' \
+--header 'Content-Type: application/json' \
+--data '{
+    "id": 1,
+    "title": "foo",
+    "body": "bar",
+    "userId": 1
+  }'
+  
+  
+  {
+    "id": 1,
+    "title": "foo",
+    "body": "bar",
+    "userId": 1
+}
+  
+ curl --location --request PATCH 'https://jsonplaceholder.typicode.com/posts/1' \
+--header 'Content-Type: application/json' \
+--data '{
+    "title": "foo"
+}'
+
+
+{
+    "userId": 1,
+    "id": 1,
+    "title": "foo",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+}
+
+curl --location --request DELETE 'https://jsonplaceholder.typicode.com/posts/1'
+
+
+{}
+    
 }
